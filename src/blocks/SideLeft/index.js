c26e1f3b365944812aabebea990b22e6dbbf4e78
legiorex <batyushev@gmail.com>
2019-06-28 22:30:36 +1000

    add postcss

--- src/blocks/SideLeft/index.js
+++ src/blocks/SideLeft/index.js
@@ -1,3 +1,4 @@
+// Core
 import React from 'react';
 import {
   faInfo,
@@ -8,11 +9,15 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// Styles
+import styles from './style.module.css';
+
 export const SideLeft = () => {
 
   return (
     <>
       <div className = 'player__menu player__menu_direction_column'>
+        <span className = { styles.test }>test</span>
         <div className = 'button player__button'>
           <FontAwesomeIcon className = 'button__icon' icon = { faInfo } />
         </div>
