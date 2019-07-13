955467992e1ac032499f85f47fa724838b83dcd5
legiorex <batyushev@gmail.com>
2019-07-13 16:40:24 +1000

    fix userbar

--- src/components/UserBar/index.js
+++ src/components/UserBar/index.js
@@ -1,6 +1,6 @@
 // Core
 import React from 'react';
-import useStoreon from 'storeon/react';
+// import useStoreon from 'storeon/react';
 
 import { faPaperclip, faHome, faComments } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -13,21 +13,19 @@ export const UserBar = () => {
   return (
     <>
       <div className = { Styles.userBar }>
-        <div className = 'player__menu player__menu_direction_row'>
-          <div className = 'button player__button'><FontAwesomeIcon className = 'button__icon' icon = { faPaperclip } /></div>
-          <div className = 'player__menu-area'>
-            <div className = 'input input_width_available'>
-              <div className = 'input__box'>
-                <textarea className = 'input__control' />
-              </div>
+        <div className = 'button player__button'><FontAwesomeIcon className = 'button__icon' icon = { faPaperclip } /></div>
+        <div className = 'player__menu-area'>
+          <div className = 'input input_width_available'>
+            <div className = 'input__box'>
+              <textarea className = 'input__control' />
             </div>
           </div>
-          <div className = 'button player__button'>
-            <FontAwesomeIcon className = 'button__icon' icon = { faHome } />
-          </div>
-          <div className = 'button player__button'>
-            <FontAwesomeIcon className = 'button__icon' icon = { faComments } />
-          </div>
+        </div>
+        <div className = 'button player__button'>
+          <FontAwesomeIcon className = 'button__icon' icon = { faHome } />
+        </div>
+        <div className = 'button player__button'>
+          <FontAwesomeIcon className = 'button__icon' icon = { faComments } />
         </div>
       </div>
     </>
